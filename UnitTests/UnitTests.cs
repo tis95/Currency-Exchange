3db27de00d2835f23acfbdbd0e068f9501e5f08a
Tal <you@example.com>
2020-08-06 00:35:00 +0300

    commit

---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using Currency_Exchange;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestInvalidOldCoin_shouldThrow()
+        {
+            Assert.ThrowsException<NullReferenceException>(() => RatesAPI.GetRateFromAPI("JJJ", "ILS"));
+        }
+        [TestMethod]
+        public void TestInvalidNewCoin_shouldThrow()
+        {
+            Assert.ThrowsException<NullReferenceException>(() => RatesAPI.GetRateFromAPI("ILS", "JJJ"));
+        }
+        [TestMethod]
+        public void TestRateSameCoin_shouldReturn1()
+        {
+            Assert.AreEqual(1, RatesAPI.GetRateFromAPI("NIS", "NIS"));
+        }
+        [TestMethod]
+        public void TestRateHigher_shouldReturnMoreThan1()
+        {
+            Assert.IsTrue(RatesAPI.GetRateFromAPI("USD", "NIS") > 1);
+        }
+        [TestMethod]
+        public void TestRateLower_shouldReturnLessThan1()
+        {
+            Assert.IsTrue(RatesAPI.GetRateFromAPI("NIS", "USD") < 1);
+        }
+    }
+}
