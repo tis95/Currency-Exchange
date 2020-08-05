84d22f7dcf509e7f181313ec94a1d4c8581080eb
Tal <you@example.com>
2020-08-05 18:57:43 +0300

    Add project files.

---
+++ Exchange/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Currency_Exchange
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            double exchangeRate = RatesAPI.GetRateFromAPI(args[0], args[1]);
+            for (int i = 2; i < args.Length; i++)
+            {
+                Console.WriteLine(exchangeRate * Double.Parse(args[i]));
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Exchange/RatesAPI.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Currency_Exchange
+{
+    public class RatesAPI
+    {
+        public static double GetRateFromAPI(String oldCurrency, String newCurrency)
+        {
+            if (oldCurrency == "NIS") oldCurrency = "ILS";
+            if (newCurrency == "NIS") newCurrency = "ILS";
+
+            String URLString = "https://prime.exchangerate-api.com/v5/2e2f0fb45768c6e39646dfad/latest/" + oldCurrency;
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = webClient.DownloadString(URLString);
+                var parsedObject = JObject.Parse(json);
+                var currencyJson = parsedObject["conversion_rates"][newCurrency].ToString();
+                return Double.Parse(currencyJson);
+            }
+
+        }
+    }
+}
